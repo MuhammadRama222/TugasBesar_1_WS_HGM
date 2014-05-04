0d48942322644dd9934832763899ffee21a02a70
MuhammadRama222 <dioramalalala@gmail.com>
2014-05-04 21:06:18 +0700

    Create sukses.php

---
+++ sukses.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 
+Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+	<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+  <link rel="stylesheet" type="text/css" href="../style.css" media="screen, tv, projection" />
+  <title>APLIKASI PEMESANAN TIKET TOL</title>
+</head>
+
+<body>
+<div id="bodyDiv">
+
+<!-- ##### Header ##### -->
+<div id="header"></div>
+
+<!-- ##### Header Close ##### -->
+<div id="headMenu">
+	<ul>
+	<li>&nbsp;<a href="../index.html">Home</a></li>
+	<li>&nbsp;<a href="../pengaduan/index.php">Pengaduan</a></li>
+	<li>&nbsp;<a href="../pengaturan bahasa/index.php">Pengaturan Bahasa</a></li>
+	<li>&nbsp;<a href="../laporan/index.php">Laporan Pemesanan</a></li>
+	</ul>
+  </div>
+<!-- ##### Content ##### -->
+
+    <p><h3><center>Aduan Anda Terkirim. Terima Kasih...
+	</center></h3></p>
+    <p>
+	untuk Kritik dan saran Bisa anda Tuliskan dalam kotak Pengaduan atau Anda dapat menghubungi kontak kami.
+	di : <a href="www.DISHUB.ac.id"> www.DISHUB.ac.id </a>&nbsp;&nbsp;&nbsp;&nbsp;<b> 66678 - 66679 </b>
+	<p><Marquee><h3>
+	TERIMA KASIH DAN SELAMAT JALAN SEMOGA SELAMAT SAMPAI TUJUAN
+	</h3></Marquee></p>
+		</div>
+		
+		
+		
+	<!-- ##### Content Close##### -->
+
+<!-- ##### Footer ##### -->
+  <div id="footer"></div>
+<!-- ##### Footer Close ##### -->
+</div>
+</div>
+</body>
+</html>
+<? include "../disconnect.php"; ?>
