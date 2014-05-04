f2118d1c7294e7f85a690c05b1f2ac4ab2b2e417
Hendra Pratama <hendrapratama0411@gmail.com>
2014-05-04 20:03:36 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$hostName = "localhost";
+$userName = "root";
+$password = " ";
+$database = "dbpengolahan_nilai";
+mysql_connect ($hostName, $password) or die ('koneksi gagal');
+mysql_select_db  ($dataBase) or die ('database tidak ditemukan');
+?>
